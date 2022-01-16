189e572f4e3e44406ba3daeca5dbe25ab194c780
Smite <SmiteDeluxe@gmail.com>
2022-01-16 22:11:25 +0100

    installed SEO tools

--- schemas/post.js
+++ schemas/post.js
@@ -4,6 +4,28 @@ export default {
   type: 'document',
   fields: [
     {
+      name: 'seo',
+      title: 'SEO',
+      type: 'seo-tools', // use seo-tools type
+      options: {
+          baseUrl: 'https://minerva-articles.sanity.studio/', // (REQUIRED) This is the baseUrl for your site
+          slug(doc) { // (REQUIRED) a function to return the slug of the current page, which will be appended to the baseUrl
+              return doc.slug.current;
+          },
+          fetchRemote: true, // Can be set to false to disable fetching the remote source (you will need to pass the content helpers for analysis)
+          title(doc) {
+              return 'page title'; // (OPTIONAL) return page title otherwise inferred from scrape
+          },
+          description(doc) {
+              return 'page description'; // (OPTIONAL) return page description otherwise inferred from scrape
+          },
+          locale(doc) {
+              return 'page locale'; // (OPTIONAL) return page locale otherwise inferred from scrape
+          },
+          contentSelector: 'body' // (OPTIONAL) option to finetune where Yoast will look for the content. (only applicable for scraping without content function)
+      },
+    },
+    {
       name: 'title',
       title: 'Title',
       type: 'string',
